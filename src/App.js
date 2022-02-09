29192013a9c8629ee10194a8ea0519090d4f4bdd
Salaudev <pysalau2909@gmail.com>
2022-02-09 09:17:18 +0500

    message

--- src/App.js
+++ src/App.js
@@ -7,58 +7,79 @@ import GlobalStyle from "./style/GlobalStyle";
 import ProductsContext from "./contexts/ProductsContext";
 import { useState } from "react";
 import ProductDetail from "./pages/ProductDetail";
+import CategoriesContext from "./contexts/CategoriesContext";
 
 function App() {
+  const [categories, setCategories] = useState([
+    "Foods",
+    "Drinks",
+    "Salats",
+    "Fruits",
+  ]);
   const [products, setProducts] = useState([
     {
       img: "https://brandvanegmond.com/wp-content/uploads/2018/12/modern-table-lamps-contemporary-lighting-arthur-collection-art30n-brandvanegmond-471x575.jpg",
       title: "Modern Table Lamps 01",
       price: 100,
+      description: "desc1",
+      category: "Drinks",
     },
     {
       img: "https://brandvanegmond.com/wp-content/uploads/2018/11/10-modern-lighting_modern-lighting-contemporary-table-lamp-lot75na-gllogrey22-light-design-louise-collection-brandvanegmond-471x421.jpg",
       title: "Modern Table Lamps 02",
       price: 200,
+      description: "desc1",
+      category: "Drinks",
     },
     {
       img: "https://brandvanegmond.com/wp-content/uploads/2018/12/modern-table-lamps-contemporary-lighting-arthur-collection-art30n-brandvanegmond-471x575.jpg",
       title: "Modern Table",
       price: 104.5,
+      description: "desc1",
+      category: "Drinks",
     },
     {
       img: "https://brandvanegmond.com/wp-content/uploads/2018/12/modern-table-lamps-contemporary-lighting-arthur-collection-art30n-brandvanegmond-471x575.jpg",
       title: "Modern Table Lamps 01",
       price: 100,
+      description: "desc1",
+      category: "Drinks",
     },
     {
       img: "https://brandvanegmond.com/wp-content/uploads/2018/11/10-modern-lighting_modern-lighting-contemporary-table-lamp-lot75na-gllogrey22-light-design-louise-collection-brandvanegmond-471x421.jpg",
       title: "Modern Table Lamps 02",
       price: 200,
+      description: "desc1",
+      category: "Drinks",
     },
     {
       img: "https://brandvanegmond.com/wp-content/uploads/2018/12/modern-table-lamps-contemporary-lighting-arthur-collection-art30n-brandvanegmond-471x575.jpg",
       title: "Modern Table",
       price: 104.5,
+      description: "desc1",
+      category: "Drinks",
     },
   ]);
 
   return (
     <ProductsContext.Provider value={{ products, setProducts }}>
-      <GlobalStyle />
-      <Header />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Main />}></Route>
-          <Route
-            path="/add"
-            element={<ProductDetail title={"Qo'shish"} />}
-          ></Route>
-          <Route
-            path="/edit/:id"
-            element={<ProductDetail title={"O'zgartirish"} />}
-          ></Route>
-        </Routes>
-      </BrowserRouter>
+      <CategoriesContext.Provider value={{ categories, setCategories }}>
+        <GlobalStyle />
+        <Header />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Main />}></Route>
+            <Route
+              path="/add"
+              element={<ProductDetail title={"Qo'shish"} />}
+            ></Route>
+            <Route
+              path="/edit/:id"
+              element={<ProductDetail title={"O'zgartirish"} />}
+            ></Route>
+          </Routes>
+        </BrowserRouter>
+      </CategoriesContext.Provider>
     </ProductsContext.Provider>
   );
 }
--- src/pages/ProductDetail/index.js
+++ src/pages/ProductDetail/index.js
@@ -5,18 +5,22 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import SqButton from "../../components/SqButton";
 import ProductsContext from "../../contexts/ProductsContext";
+import CategoriesContext from "../../contexts/CategoriesContext";
 import Card from "../../components/Card";
 import { useForm } from "react-hook-form";
 import { useNavigate, useParams } from "react-router-dom";
 
 export default function ProductDetail({ title }) {
   const { products, setProducts } = useContext(ProductsContext);
+  const { categories, setCategories } = useContext(CategoriesContext);
 
+  console.log(products);
+  console.log(categories);
   const params = useParams();
   console.log(params);
   const navigate = useNavigate();
 
-  const index = params.id;
+  const index = params.category;
 
   const { register, reset, handleSubmit } = useForm({
     defaultValues: index ? products[index] : {},
@@ -24,21 +28,25 @@ export default function ProductDetail({ title }) {
 
   const submit = (data) => {
     const t = [...products];
+    console.log(data);
 
     if (index) {
-      t[index] = data;
+      // products.push(data);
+      setProducts(...products, data);
+      console.log(products);
+
+      // t[index] = data;
     } else {
-      t.push(data);
+      // t.push(data);
+      categories.push(data);
+      setCategories(data);
     }
 
-    setProducts(t);
     navigate("/");
   };
 
   return (
     <Wrapper className="text-center container">
-      <Title title={title} />
-
       <form onSubmit={handleSubmit(submit)}>
         <div className="row mb-5">
           <div className="col-md-6 mb-3">
@@ -55,17 +63,37 @@ export default function ProductDetail({ title }) {
               {...register("title", { required: true })}
             ></input>
           </div>
+          <div className="col-md-12 mb-3">
+            <textarea
+              className="form-control textArea w-100"
+              placeholder="Tarif"
+              type="number"
+              {...register("description", { required: true })}
+            ></textarea>
+          </div>
           <div className="col-md-6 mb-3">
             <input
               className="form-control w-100"
-              placeholder="Mahsulot narxi"
-              type="number"
+              placeholder="Narxi"
               {...register("price", { required: true })}
             ></input>
           </div>
+          <div className="col-md-6 mb-3">
+            <select
+              className="form-control w-100"
+              placeholder="Mahsulot nomi"
+              name="category"
+              {...register("category", { required: true })}
+            >
+              {categories.map((item, i) => (
+                <option key={i} className="p-2">
+                  {item}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
         <div className="text-center">
-          <button className="btn btn-dark me-3 styledBtn">Ortga qaytish</button>
           <button className="btn btn-dark styledBtn" type="submit">
             Saqlash
           </button>
